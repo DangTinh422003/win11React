4d05bb955065526e8a213788df6e0b94902d1ef8
Dang Tinh <caodangtinh04022003@gmail.com>
2024-10-06 01:22:37 +0700

    update: bubble ui

--- src/containers/applications/apps/bubblehub.jsx
+++ src/containers/applications/apps/bubblehub.jsx
@@ -156,79 +156,14 @@ export const BubbleHub = () => {
         name="Bubble Hub"
       />
       <div
-        className="bg-white windowScreen flex flex-col min-w-full"
+        className="bg-white windowScreen flex flex-col min-w-full h-screen"
         data-dock="true"
       >
-        <div className="container mx-auto mt-4 p-4">
-          <h1 className="text-2xl font-medium">Environments</h1>
-          <div className="mt-8 mb-4 h-10 px-4 flex items-center gap-2 border border-solid border-[#ddddde] rounded-md w-full lg:w-80">
-            <input type="text" className="flex-1 h-full" placeholder="Search" />
-            <IoIosSearch className="size-6 cursor-pointer inline-block font-bold" />
-          </div>
-
-          <div className="border border-solid border-[#ddddde] rounded-md text-sm">
-            <ul className="font-semibold grid grid-cols-4 px-4 h-14">
-              <li className="flex items-center">Name</li>
-              <li className="flex items-center">Owner</li>
-              <li className="col-span-2 flex items-center">Systems</li>
-            </ul>
-
-            <div>
-              {mockData.map((data) => (
-                <ul
-                  className="border-b border-t grid grid-cols-4 p-4 hover:bg-gray-100 cursor-pointer group"
-                  key={data.owner.address}
-                >
-                  <li className="flex items-center font-medium text-sm">
-                    {data.name}
-                  </li>
-                  <li className="flex gap-3 items-center">
-                    <img
-                      src={data.owner.avatar}
-                      className="size-10 rounded-full overflow-hidden"
-                      alt=""
-                    />
-                    <div className="flex flex-col">
-                      <p className="font-semibold">{data.owner.name}</p>
-                      <p className="font-medium text-gray-600">
-                        {data.owner.email}
-                      </p>
-                    </div>
-                  </li>
-                  <li className="col-span-2 ">
-                    <div className="flex justify-between">
-                      <div className="w-2/3 flex h-full items-center flex-wrap gap-2">
-                        {data.systems.map((system) => (
-                          <span
-                            key={system}
-                            className="text-xs px-2 py-1 rounded-md border border-solid border-[#ddddde]"
-                          >
-                            {system}
-                          </span>
-                        ))}
-                      </div>
-                      <ActivityPopup />
-                    </div>
-                  </li>
-                </ul>
-              ))}
-            </div>
-
-            <div className="p-4 flex items-center justify-end">
-              <div className="flex gap-10">
-                <p>1 - 1 of 1</p>
-                <div className="flex gap-6 items-center">
-                  <button>
-                    <FaChevronLeft />
-                  </button>
-                  <button>
-                    <FaChevronRight />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <iframe
+          className="w-full h-full"
+          src="https://bubble-hub.vercel.app/"
+          frameborder="0"
+        ></iframe>
       </div>
     </div>
   );
